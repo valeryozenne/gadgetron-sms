c27d3e8cb97713c1933d4d7b35857105c9151d54
Benoit <benoit.tachet@epitech.eu>
2020-07-06 13:15:43 +0200

    Added FFT to ZeroFilling

---
+++ apps/standalone/gpu/zerofilling_memcpy/CMakeFiles/zerofilling_run_gpu.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/zerofilling_run_gpu.dir/zerofilling.o"
+  "zerofilling_run_gpu"
+  "zerofilling_run_gpu.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/zerofilling_run_gpu.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
